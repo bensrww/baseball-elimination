2b3df37dc82602934b69f1e8149e29b4af753ea3
Ben Chong <bensrww@gmail.com>
2022-09-12 02:51:25 +0800

    add BaseballElimination constructor

--- BaseballElimination.java
+++ BaseballElimination.java
@@ -4,16 +4,43 @@
  *  Description:
  **************************************************************************** */
 
+import edu.princeton.cs.algs4.In;
+
 public class BaseballElimination {
+
+    private int _numOfTeams;
+    private String[] _teams;
+    private int[] _wins;
+    private int[] _loses;
+    private int[] _left;
+    private int[][] _against;
+
     // create a baseball division from given filename in format specified below
     public BaseballElimination(
             String filename) {
+        In in = new In(filename);
+        _numOfTeams = in.readInt();
 
+        this._teams = new String[_numOfTeams];
+        this._wins = new int[_numOfTeams];
+        this._loses = new int[_numOfTeams];
+        this._left = new int[_numOfTeams];
+        this._against = new int[_numOfTeams][_numOfTeams];
+
+        for (int teamId = 0; teamId < _numOfTeams; teamId += 1) {
+            _teams[teamId] = in.readString();
+            _wins[teamId] = in.readInt();
+            _loses[teamId] = in.readInt();
+            _left[teamId] = in.readInt();
+            for (int iii = 0; iii < _numOfTeams; iii += 1) {
+                _against[teamId][iii] = in.readInt();
+            }
+        }
     }
 
     // number of teams
     public int numberOfTeams() {
-        return 0;
+        return _numOfTeams;
     }
 
     // all teams
@@ -52,6 +79,7 @@ public class BaseballElimination {
     }
 
     public static void main(String[] args) {
-
+        BaseballElimination be = new BaseballElimination(args[0]);
+        System.out.println(be.numberOfTeams());
     }
 }
