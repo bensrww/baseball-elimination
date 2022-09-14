3724411299d7e9f42257f7ad7f21f98212c06cb3
Ben Chong <bensrww@gmail.com>
2022-09-15 01:11:02 +0800

    add nodeMap

--- BaseballElimination.java
+++ BaseballElimination.java
@@ -14,6 +14,18 @@ import java.util.Objects;
 
 public class BaseballElimination {
 
+    class NodeMap {
+        int _nodeId;
+        int _teamLeft;
+        int _teamRight;
+
+        NodeMap(int id, int teamLeft, int teamRight) {
+            _nodeId = id;
+            _teamLeft = teamLeft;
+            _teamRight = teamRight;
+        }
+    }
+
     private int _numOfTeams;
     private String[] _teams;
     private int[] _wins;
@@ -22,18 +34,21 @@ public class BaseballElimination {
     private int[][] _against;
     private FlowNetwork[] _network;
     private FordFulkerson[] _fordFulkerson;
+    private NodeMap[][] _nodeMap;
 
     // create a baseball division from given filename in format specified below
-    public BaseballElimination(
-            String filename) {
+    public BaseballElimination(String filename) {
         In in = new In(filename);
-        _numOfTeams = in.readInt();
+        this._numOfTeams = in.readInt();
 
         this._teams = new String[_numOfTeams];
         this._wins = new int[_numOfTeams];
         this._loses = new int[_numOfTeams];
         this._left = new int[_numOfTeams];
         this._against = new int[_numOfTeams][_numOfTeams];
+        this._network = new FlowNetwork[_numOfTeams];
+        this._fordFulkerson = new FordFulkerson[_numOfTeams];
+        this._nodeMap = new NodeMap[_numOfTeams][getMaxNumOfNodes()];
 
         for (int teamId = 0; teamId < _numOfTeams; teamId += 1) {
             _teams[teamId] = in.readString();
@@ -46,20 +61,60 @@ public class BaseballElimination {
         }
 
         for (int teamId = 0; teamId < _numOfTeams; teamId += 1) {
+            int teamLeftId = 0;
+            int teamRightId = 0;
+            int nodeId = 1;
+
+            while (nodeId <= getTeamComboCutoff()) {
+                if (teamLeftId >= _numOfTeams) {
+                    break;
+                }
+                if (teamLeftId == teamRightId || teamLeftId == teamId || teamRightId == teamId) {
+                    if (teamRightId < _numOfTeams) {
+                        teamRightId += 1;
+                    }
+                    else if (teamRightId == _numOfTeams) {
+                        teamLeftId += 1;
+                        teamRightId = 0;
+                    }
+                    continue;
+                }
+
+                _nodeMap[teamId][nodeId] = new NodeMap(nodeId, teamLeftId, teamRightId);
+
+                if (teamRightId < _numOfTeams) {
+                    teamRightId += 1;
+                }
+                else if (teamRightId == _numOfTeams) {
+                    teamLeftId += 1;
+                    teamRightId = 0;
+                }
+                nodeId += 1;
+            }
+        }
+
+        for (int teamId = 0; teamId < _numOfTeams; teamId += 1) {
             // construct network without team[teamId]
-            FlowNetwork flowNetwork = new FlowNetwork(
-                    2 + _numOfTeams - 1 + (_numOfTeams - 1 + _numOfTeams - 2) / 2);
-            // 0 is source, {1 --- (_numOfTeams - 1 + _numOfTeams - 2) / 2}(6) is team matches,
-            // { (1 + cutoff) }(7) --- { (1 + cutoff) + _numOfTeams - 1 }(10) is teams
-            // {1 + cutoff + _numOfTeams}(11) is target
+            FlowNetwork flowNetwork = new FlowNetwork(getMaxNumOfNodes());
+
             for (int iii = 1; iii < getTeamComboCutoff(); iii += 1) {
                 flowNetwork.addEdge(new FlowEdge(0, iii, Double.POSITIVE_INFINITY));
             }
 
             for (int iii = 1; iii < getTeamComboCutoff(); iii += 1) {
-                flowNetwork.addEdge(new FlowEdge(0, iii, Double.POSITIVE_INFINITY));
+                flowNetwork.addEdge(
+                        new FlowEdge(iii, getTeamComboCutoff() + 1, Double.POSITIVE_INFINITY));
+                flowNetwork.addEdge(
+                        new FlowEdge(iii, getTeamComboCutoff() + 2, Double.POSITIVE_INFINITY));
             }
         }
+    }
+
+    private int getMaxNumOfNodes() {
+        // 0 is source, {1 --- (_numOfTeams - 1 + _numOfTeams - 2) / 2}(6) is team matches,
+        // { (1 + cutoff) }(7) --- { (1 + cutoff) + _numOfTeams - 1 }(10) is teams
+        // {1 + cutoff + _numOfTeams}(11) is target
+        return 2 + _numOfTeams - 1 + (_numOfTeams - 1 + _numOfTeams - 2) / 2;
     }
 
     private int getTeamComboCutoff() {
